b7677ad3eb680f25595e4b6378e5415f5f7369ab
arsh-gupta-8 <SlurpIt.MBMS@gmail.com>
2024-03-06 19:51:26 +0700

    made one cell at a time selectable and unselectable and changed game format

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ FPS = 30
 
 # Colour constant variables
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+SUPER_LIGHT_GREY = (211, 211, 211)
 LIGHT_GREY = (118, 118, 118)
 DARK_GREY = (64, 64, 64)
+BLACK = (0, 0, 0)
 
 # Screen management
-screen = pygame.display.set_mode((482, 557))
+screen = pygame.display.set_mode((902, 582))
 pygame.display.set_caption('Sudoku Game')
 screen.fill(WHITE)
 
@@ -27,18 +28,30 @@ for row in range(9):
     for column in range(9):
         grid[row].append(0)
 
+# Game settings
+place_holder_rect = None
+
+
+# Update screen
+def window_update(grid_rect):
+    screen.fill(WHITE)
+    set_dimensions()
+    if grid_rect is not None:
+        pygame.draw.rect(screen, SUPER_LIGHT_GREY, grid_rect)
+    pygame.display.update()
+
 
 def set_dimensions():
     # Draws smaller lines between every line
     for line_x in range(3):
-        pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(line_x * 159 + 55, 75, 54, 482), 2)
+        pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(50 + line_x * 159 + 55, 50, 54, 482), 2)
     for line_y in range(3):
-        pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(0, line_y * 159 + 130, 482, 54), 2)
+        pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(50, line_y * 159 + 105, 482, 54), 2)
 
     # Draws bold lines between each set of 9 squares
-    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(0, 75, 482, 482), 5)
-    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(159, 75, 164, 482), 5)
-    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(0, 234, 482, 164), 5)
+    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(50, 50, 482, 482), 5)
+    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(209, 50, 164, 482), 5)
+    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(50, 209, 482, 164), 5)
 
 
 def find_box_dim(x_val, y_val):
@@ -46,16 +59,16 @@ def find_box_dim(x_val, y_val):
     y_box = -1
     addition = 0
     for box in range(9):
-        if x_val >= 5 + 50 * box + addition:
+        if x_val >= 55 + 50 * box + addition:
             x_box += 1
-        if y_val >= 80 + 50 * box + addition:
+        if y_val >= 55 + 50 * box + addition:
             y_box += 1
         addition += 2
         if box % 3 == 2:
             addition += 3
 
-    a = x_box * 50 + (x_box // 3) * 3 + x_box * 2 + 5
-    b = y_box * 50 + (y_box // 3) * 3 + y_box * 2 + 80
+    a = x_box * 50 + (x_box // 3) * 3 + x_box * 2 + 55
+    b = y_box * 50 + (y_box // 3) * 3 + y_box * 2 + 55
 
     return a, b
 
@@ -74,11 +87,11 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 colour_clicked = screen.get_at((x, y))[:3]
-                if 5 <= x <= 477 and 80 <= y <= 552 and colour_clicked != DARK_GREY and colour_clicked != LIGHT_GREY:
-                    print("inside")
+                if 55 <= x <= 527 and 55 <= y <= 527 and colour_clicked not in [DARK_GREY, LIGHT_GREY, SUPER_LIGHT_GREY]:
                     x_place, y_place = find_box_dim(x, y)
-                    pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(x_place, y_place, 50, 50))
+                    place_holder_rect = pygame.Rect(x_place, y_place, 50, 50)
+                elif colour_clicked == SUPER_LIGHT_GREY:
+                    place_holder_rect = None
 
-    set_dimensions()
-    pygame.display.update()
+    window_update(place_holder_rect)
     clock.tick(FPS)
